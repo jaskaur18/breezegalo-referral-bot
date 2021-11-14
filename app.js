264d46e26849c0277c6d96b859b63dc534769162
jaskaur18 <jaskaurhello@gmail.com>
2021-11-15 01:05:34 +0530

    comments

--- app.js
+++ app.js
@@ -16,31 +16,48 @@ bot.use(session());
 bot.use(stage.middleware());
 ////////////////////////////////////////////////////////////////
 
+
+//referral start command
 bot.hears(/^\/start (.+[1-9]$)/, Referral_start());
 
-
+//start command
 bot.start(start_command());
 
+//change wallet command
 bot.command("changewallet", changewalletCommand());
 
+//JoinFirst Group
 bot.action("join_bot", join1);
+
+//join second Group
 
 bot.action("join_bot2", join2);
 
+
+//check second Group
+
 bot.action("join_bot3", join3);
 
+//add wallet
 bot.action("addwallet", setwallet)
 
+//leaderboard button
 bot.hears(/Leaderboard 📊/, dashboard())
 
+//my point button
 bot.hears(/My Points 🎖/, mypoint())
 
+//my wallet button
 bot.hears(/My Wallet Address 💰/, wallet())
 
+//shill message button english
 bot.hears(/Shill Message 🇬🇧/, shillen())
 
+//shill messae button chinise
 bot.hears(/推广文案 🇨🇳/, shillcn())
 
+//change add wallet handler
 addwallet.on("message", addwalletHandler())
 
+//bot start
 bot.launch(console.log("Bot Starting Working"))
