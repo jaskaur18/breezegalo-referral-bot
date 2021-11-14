03907c3d55866d0a60b584531a4f0b1b01b4e42b
jaskaur18 <jaskaurhello@gmail.com>
2021-11-15 00:56:39 +0530

    wallet fix

--- actions/join3.js
+++ actions/join3.js
@@ -1,8 +1,8 @@
 const { errorHandler } = require("../helpers")
 const { Keyboard, Key } = require("telegram-keyboard");
 
-addwalletKeyboard = Keyboard.make([Key.callback("Add Wallet Addess", "addwallet")]).inline();
 
+keyboard = Keyboard.make([["Leaderboard 📊"], ["My Points 🎖"], ["My Wallet Address 💰"], ['Shill Message 🇬🇧', "推广文案 🇨🇳"]]).reply();
 
 module.exports = async (ctx) => {
     joined = await ctx.telegram.getChatMember(process.env.Group_id_two ,ctx.from.id );
@@ -11,6 +11,7 @@ module.exports = async (ctx) => {
         return ctx.answerCbQuery("You Didn't Joined Group!!! ❌", (alert = true));
     ctx.deleteMessage().catch((err) => console.log(err));
     ctx.answerCbQuery("You Joined Group!!! ✅", (alert = true)).catch(err => errorHandler(err, ctx))
-    ctx.reply("Last Step Added Wallet Address To Continue", addwalletKeyboard)
+    await ctx.reply("Message Here Explain About The Project", keyboard).catch(err => errorHandler(err, ctx))
+
 
 }
